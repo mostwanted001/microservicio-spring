b3b558bbeae1f03a1f8d2172f63a9ca6c8b1ef26
Alexis Rodriguez Aedo <alexis.rodriguez@femsasalud.com>
2023-05-21 23:06:11 -0400

    change interface

--- src/test/java/com/example/testingweb/HttpRequestTest.java
+++ src/test/java/com/example/testingweb/HttpRequestTest.java
@@ -22,6 +22,6 @@ public class HttpRequestTest {
 	@Test
 	public void greetingShouldReturnDefaultMessage() throws Exception {
 		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
-				String.class)).contains("hello, world");
+				String.class)).contains("Finaliza el curso DevOps Foundation v4 - Alexis");
 	}
 }
